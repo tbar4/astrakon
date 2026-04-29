56d8ce3e1ce460395f431948761cba088d510768
Trevor Barnes <trevor.barnes91@gmail.com>
2026-04-28 18:22:43 -0700

    fix: add .gitignore, tighten pytest-asyncio version, auto package discovery

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
 @pytest.fixture
-def sample_faction_id():
+def sample_faction_id() -> str:
     return "ussf"
 
 @pytest.fixture
-def sample_turn():
+def sample_turn() -> int:
     return 1
